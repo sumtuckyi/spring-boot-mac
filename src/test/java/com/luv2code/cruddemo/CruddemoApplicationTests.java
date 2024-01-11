bec5fa6ae73f6bf6959e789f7a6053a82c1514fa
sumin <sumin510@naver.com>
2024-01-11 17:52:07 +0900

    1월 11일

--- src/test/java/com/luv2code/cruddemo/CruddemoApplicationTests.java
+++ src/test/java/com/luv2code/cruddemo/CruddemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.cruddemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringcoredemoApplicationTests {
+class CruddemoApplicationTests {
 
 	@Test
 	void contextLoads() {
